9f45b2d19b45a4d7a971a95a2913b0171879dd8f
marinakos <marinamplampla@hotmail.gr>
2021-03-16 21:32:25 +0100

    updating our application with reselect, replaced cart selectors with memoized selectors

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -7,7 +7,8 @@ import { addItem } from '../../redux/cart/cart.actions';
 import './collection-item.styles.scss';
 
 const CollectionItem = ({ item, addItem }) => {
-    const {id, name, price, imageUrl} = item;
+    const { name, price, imageUrl} = item;
+
     return (
     <div className='collection-item'>
         <div 
@@ -20,14 +21,15 @@ const CollectionItem = ({ item, addItem }) => {
           <span className='name'>{name}</span>
           <span className='price'>{price}</span>
         </div>
-        <CustomButton onClick={()=> addItem(item)} inverted> 
+        <CustomButton onClick={() => addItem(item)} inverted> 
         Add to cart 
         </CustomButton>
     </div>
-)};
+  );
+};
 
 const mapDispatchToProps = dispatch => ({
     addItem: item => dispatch(addItem(item))
-}) 
+}); 
 
 export default connect(null, mapDispatchToProps)(CollectionItem);
